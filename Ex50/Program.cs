0742958b1388f172892d7682489d35898cbffeb6
Sergey Kulanin <sjulanin1981@gmail.com>
2023-03-19 21:31:04 +0300

    Найти значение элемента в двумерном массиве

---
+++ Ex50/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает индексы элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Console.WriteLine("введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [10,10];
+FillArrayRandomNumbers(numbers);
+
+if (rows > numbers.GetLength(0) || columns > numbers.GetLength(1))
+{
+    Console.WriteLine("такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"значение элемента {rows} строки и {columns} столбца равно {numbers[rows-1,columns-1]}");
+}
+
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.WriteLine(""); 
+    }
+}
